5a477a62602ea828de2cb571804ad7ccc307299b
Clarence Chan <clarencechaan@icloud.com>
2022-08-24 16:48:20 -0400

    linked-list

---
+++ recursion-single-linked-list.js
@@ -0,0 +1,29 @@
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null,
+      },
+    },
+  },
+};
+
+function iterativePrintList(list) {
+  let curr = list;
+
+  while (curr) {
+    console.log(curr.value);
+    curr = curr.next;
+  }
+}
+
+function recursivePrintList(list) {
+  if (list) {
+    console.log(list.value);
+    recursivePrintList(list.next);
+  }
+}

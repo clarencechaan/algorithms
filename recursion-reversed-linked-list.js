abc0fd4d242e54a1fe18a0dcb2c358222d6ce503
Clarence Chan <clarencechaan@icloud.com>
2022-08-24 17:07:15 -0400

    reversed linked list

---
+++ recursion-reversed-linked-list.js
@@ -0,0 +1,34 @@
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null,
+      },
+    },
+  },
+};
+
+function iterativePrintReversedList(list) {
+  let tmp = list;
+  let arr = [];
+
+  while (tmp) {
+    arr.push(tmp.value);
+    tmp = tmp.next;
+  }
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    console.log(arr[i]);
+  }
+}
+
+function recursivePrintReversedList(list) {
+  if (list) {
+    recursivePrintReversedList(list.next);
+    console.log(list.value);
+  }
+}

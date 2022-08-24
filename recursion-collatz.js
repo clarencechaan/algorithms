99caafcd9e7aab5b7cd21c5268c43e8f5a6501f9
Clarence Chan <clarencechaan@icloud.com>
2022-08-24 17:34:22 -0400

    collatz

---
+++ recursion-collatz.js
@@ -0,0 +1,9 @@
+function collatz(n) {
+  if (n == 1) {
+    return 0;
+  } else if (n % 2 === 0) {
+    return 1 + collatz(n / 2);
+  } else if (n % 2 === 1) {
+    return 1 + collatz(3 * n + 1);
+  }
+}
